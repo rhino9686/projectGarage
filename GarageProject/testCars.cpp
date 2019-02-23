22924a6e6802c685b462e03ed7e8171070e1971d
Robert Cecil <rhinobobby@gmail.com>
2019-02-23 12:03:09 -0500

    Added tests to make sure error checking was working with basic input errors

--- GarageProject/testCars.cpp
+++ GarageProject/testCars.cpp
@@ -38,7 +38,11 @@ public:
     void carTestValidParams2();
     void carTestuniqID();
     void carTestPrintCar();
-    void carTestInvalidParams1();
+    void carTestInvalidMake();
+    void carTestInvalidModel();
+    void carTestInvalidYear();
+    void carTestInvalidMPG();
+    void carTestInvalidSpeed();
     
 };
 
@@ -49,6 +53,11 @@ int main() {
     carTests.carTestValidParams2();
     carTests.carTestuniqID();
     carTests.carTestPrintCar();
+    carTests.carTestInvalidMake();
+    carTests.carTestInvalidModel();
+    carTests.carTestInvalidYear();
+    carTests.carTestInvalidMPG();
+    carTests.carTestInvalidSpeed();
     
     return 0;
 }
@@ -113,9 +122,10 @@ void CarTester::carTestuniqID() {
     Cars.push_back(copiedCar2);
     Cars.push_back(copiedCar3);
     
-    for (Car* car: Cars) {
+   /* for (Car* car: Cars) {  //optional printing
         printf("%d \n",car->getID());
     }
+    */
     
     assert(copiedCar3->getID() - uniqCar1->getID() == 5);
     printf("Car uniq IDs are incremementing properly.\n" );
@@ -139,10 +149,112 @@ void CarTester::carTestPrintCar() {
     printf("Cars are printing properly.\n" );
 }
 
-void CarTester::carTestInvalidParams1() {
+void CarTester::carTestInvalidMake() {
     
+    make = "";
+    model = "Civic";
+    type = carType::SEDAN;
+    year = 2010;
+    speed = 23;
+    mpg = 32;
     
+    constructionError error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Make_error);
+    make = "grfrggggrrgrgrgrgrg";
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Make_error);
     
-    printf("Error checking working!\n" );
+    make = "Honda";
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::none);
+    
+    printf("Invalid make was caught properly.\n" );
+}
+
+void CarTester::carTestInvalidModel() {
+    
+    make = "Honda";
+    model = "";
+    type = carType::SEDAN;
+    year = 2010;
+    speed = 23;
+    mpg = 32;
+    
+    constructionError error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Model_error);
+    model = "grfrggggrrgrgrgrgrg";
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Model_error);
+    
+    model = "Civic";
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::none);
+    
+    printf("Invalid model was caught properly.\n" );
+}
+
+void CarTester::carTestInvalidYear() {
+    
+    make = "Honda";
+    model = "Civic";
+    type = carType::SEDAN;
+    year = 1800;
+    speed = 23;
+    mpg = 32;
+    
+    constructionError error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Year_error);
+    year = 230403;
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Year_error);
+    
+    year = 1980;
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::none);
+    
+    printf("Invalid year was caught properly.\n" );
+}
+
+void CarTester::carTestInvalidMPG() {
+    
+    make = "Honda";
+    model = "Civic";
+    type = carType::SEDAN;
+    year = 2010;
+    speed = 23;
+    mpg = -2;
+    
+    constructionError error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::MPG_error);
+    mpg = 7000;
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::MPG_error);
+    
+    mpg = 34;
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::none);
+    
+    printf("Invalid mpg was caught properly.\n" );
+}
+
+void CarTester::carTestInvalidSpeed() {
+    make = "Chevrolet";
+    model = "F150";
+    type = carType::TRUCK;
+    year = 2010;
+    speed = -2; //expected to be invalid
+    mpg = 12;
+    
+    constructionError error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Speed_error);
+    speed = 2344339; //also expected to be invalid
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::Speed_error);
+    
+    speed = 70;
+    error =  Car::checkError(make, model, type, mpg, year, speed);
+    assert(error == constructionError::none);
+    
+    printf("Invalid speed was caught properly.\n" );
 }
 
