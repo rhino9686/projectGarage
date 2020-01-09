8d394917e096ef7a0f6d265e05ad3ae6d16081ee
Robert Cecil <robcecil@umich.edu>
2020-01-08 21:37:19 -0500

    removed bloat

--- GarageProject-Python/main.py
+++ GarageProject-Python/main.py
@@ -11,66 +11,87 @@ def print_options(gar):
     print(my_str)
     return None
 
-# verbosity controls how much output the user gets after each action
+# Toggles verbosity
+# Verbosity controls how much output the user gets after each action
 def toggle_verbosity(gar):
     print("toggling Verbosity")
     global verbose
     verbose = not verbose
     print ("Verbose: " + str(verbose))
 
-# adds a car to the database
+# Adds a car to the database
 def add_car(gar):
     
     print("What type of car?\n")
+    # cast character to lower case so that it's not case sensitive
     user_choice = input("   A. SUV \n B. Sedan \n C. Truck \n D. Crossover: \n E. Other: \n").lower()
+    # if user enters any other character than A-D it will be "other"
     select_type = type_map.get(user_choice, CarType.OTHER)
+    
+    make_model_str = input("What year, make, and model is your car? Enter year, make, then model, with spaces in between: ")
 
-    make_model_str = input("What year, make, and model is your car? Enter year, make, then model, with spaces in between: \n")
+    try:
+        select_year, select_make, select_model = make_model_str.split(" ")
+    except:
+        packed = make_model_str.split(" ")
+        select_year = packed[0]
+        select_make = packed[1]
+        select_model = packed[2]
 
-    select_year, select_make, select_model = make_model_str.split(" ")
-
-    select_speed = input ("What is the max speed?\n")
-    select_mpg  = input("What is the average MPG?\n")
+    select_speed = input("What is the max speed?")
+    select_mpg  = input("What is the average MPG?")
 
     print("thanks! Processing...")
 
+
+    # error check to make sure all values are correct and reasonable
+
+
+
+    # create car model, add it to the garage
     car_entry = Car(select_make,select_model,select_type,select_year,select_speed,select_mpg)
+    gar.add_car(car_entry)
 
-    print("Adding car")
+    print("Added car successfully!")
 
-# adds multiple cars to database from an input file
-# if any errors pop up, doesn't add any car
+
+# Adds multiple cars to database from an input file
+# If any errors pop up, doesn't add any car
 def add_cars_from_file(gar):
     print("adding cars from file")
 
-# simulates a race of all cars in database
+# Simulates a race of all cars in database
 def race_cars(gar):
     print("racing cars")
 
-# empties database of all cars
+# Empties database of all cars
 def clear_cars(gar):
     print("clearing cars")
 
-# lists all cars in database
+# Lists all cars in database
 def list_cars(gar):
     print("listing cars")
 
-# lists stats about fastest car, most efficient, etc.
+# Lists stats about fastest car, most efficient, etc.
 def get_data(gar):
     print("Getting data about cars")
 
-# allows user to make traditional SQL-style queries on database
+# Allows user to make traditional SQL-style queries on database
 def query_data(gar):
     print("querying data from cars")
 
-
+# Prints out a giant help message to list all the commands
 def help_msg(gar):
     print("Help Message!")
-
+# Quits the program
 def quit_program(gar):
     print("Goodbye!")
     global quit_command
     quit_command = True
+
+# Default function to hit whenever giberrish is entered
+def print_unrecognized(gar):
+    print("unrecognized command!")
 
 
 func_map = {
@@ -97,11 +118,8 @@ def main():
     while not quit_command:
         command_func = None
         user_command = input("What do you want to do next? \n")
-        command_func = func_map.get(user_command.lower(), None)
-        if command_func:
-            command_func(gar)
-        else:
-            print("unrecognized command!")
+        command_func = func_map.get(user_command.lower(), print_unrecognized)
+        command_func(gar)
 
 
 if __name__ == '__main__':
